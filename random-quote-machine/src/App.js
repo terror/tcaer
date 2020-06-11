94d7a728227506213a6bcbe36fcf36b4c9384e28
Liam <terroridk@gmail.com>
2020-06-10 22:24:57 -0400

    add random quote machine

---
+++ random-quote-machine/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// CSS
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Components
+import QuoteBox from './components/QuoteBox';
+
+const App = () => {
+	return (
+		<div className='App'>
+			<QuoteBox />
+		</div>
+	);
+};
+
+export default App;
